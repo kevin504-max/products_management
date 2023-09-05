9f8728b027fb3a4cd95b670cfa677f81b92986a0
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-09-04 23:35:01 -0300

    refact: correction on display cards

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,42 +8,43 @@
             Bem vindo ao Dashboard de Gerenciamento de Conteúdo
         </h1>
 
-        <div class="content mt-5 col-md-4">
-            @if ($products->count() > 0)
-                @foreach ($products as $product)
-                    <form>
-                        @csrf
-                        <div class="card shadow mt-2 product_data">
-                            <input type="hidden" class="product_id" value="{{ $product->id }}">
-                            <div class="card-header">
-                                <h3>{{ $product->name }}</h3>
-                            </div>
+        <div class="content mt-5">
+            <div class="row">
+                @if ($products->count() > 0)
+                    @foreach ($products as $product)
+                        <div class="col-md-4">
+                            <form>
+                                @csrf
+                                <div class="card shadow mt-2 product_data">
+                                    <input type="hidden" class="product_id" value="{{ $product->id }}">
+                                    <div class="card-header">
+                                        <h3>{{ $product->name }}</h3>
+                                    </div>
 
-                            <div class="card-body">
-                                <p><strong>Código: </strong> {{ $product->code }}</p>
-                                <p><strong>Valor: </strong>R$ {{ number_format($product->price, 2, ',', '.') }}</p>
-                            </div>
+                                    <div class="card-body">
+                                        <p><strong>Código: </strong> {{ $product->code }}</p>
+                                        <p><strong>Valor: </strong>R$ {{ number_format($product->price, 2, ',', '.') }}</p>
+                                    </div>
 
-                            <div class="card-footer">
-                                <div class="input-group text-center justify-content-center mb-3" style="width: 120px;">
-                                    <span class="input-group-prepend bg-white border-0 mt-3">
-                                        <button class="btn btn-sm btn-outline-secondary decrement-btn" id="decrement-btn" type="button">-</button>
-                                    </span>
-                                    <input type="text" class="form-control text-center border-0 qty-input" id="quantity" name="quantity" value="1">
-                                    <span class="input-group-append bg-white border-0 mt-3">
-                                        <button class="btn btn-sm btn-outline-secondary increment-btn" id="increment-btn" type="button">+</button>
-                                    </span>
+                                    <div class="card-footer">
+                                        <div class="input-group text-center justify-content-center mb-3" style="width: 120px;">
+                                            <span class="input-group-prepend bg-white border-0 mt-3">
+                                                <button class="btn btn-sm btn-outline-secondary decrement-btn" id="decrement-btn" type="button">-</button>
+                                            </span>
+                                            <input type="text" class="form-control text-center border-0 qty-input" id="quantity" name="quantity" value="1">
+                                            <span class="input-group-append bg-white border-0 mt-3">
+                                                <button class="btn btn-sm btn-outline-secondary increment-btn" id="increment-btn" type="button">+</button>
+                                            </span>
+                                        </div>
+                                    </div>
+
+                                    <button class="btn btn-primary btn-md p-1 m-3 addCartBtn" type="button">Adicionar ao carrinho</button>
                                 </div>
-
-                            </div>
-
-                            <button class="btn btn-primary btn-md p-1 m-3 addCartBtn" type="button">Adicionar ao carrinho</button>
+                            </form>
                         </div>
-                    </form>
-
-                    <hr>
-                @endforeach
-            @endif
+                    @endforeach
+                @endif
+            </div>
         </div>
     </div>
 
